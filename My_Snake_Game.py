55bfc409e658299748f57285043bd04f32bda191
leratorantekane <54166528+leratorantekane@users.noreply.github.com>
2023-05-30 17:09:39 +0200

    Add files via upload

--- My_Snake_Game.py
+++ My_Snake_Game.py
@@ -1,11 +1,11 @@
 import random
+from token import AMPER
 import turtle
 import time
 
-# scores
+# reset score and delay
 my_score = 0
 high_scores = 0
-
 delay = 0.1
 
 # screen setup
@@ -16,13 +16,13 @@ screen.setup(width=700, height=700)
 screen.tracer(0)
 
 # the snake head
-my_snake = turtle.Turtle()
-my_snake.speed(0)
-my_snake.shape("circle")
-my_snake.color("blue")
-my_snake.penup()
-my_snake.goto(0, 0)
-my_snake.direction = "stop"
+snake_head = turtle.Turtle()
+snake_head.speed(0)
+snake_head.shape("circle")
+snake_head.color("blue")
+snake_head.penup()
+snake_head.goto(0, 0)
+snake_head.direction = "stop"
 
 # the snake's food
 prey = turtle.Turtle()
@@ -31,8 +31,6 @@ prey.shape("square")
 prey.color("green")
 prey.penup()
 prey.goto(0, 100)
-
-segments = []
 
 # scoreboards
 board = turtle.Turtle()
@@ -44,39 +42,43 @@ board.hideturtle()
 board.goto(0, 260)
 board.write("Score: 0 High Scores: 0", align="center", font=("ds-digital", 24, "normal"))
 
-# functionality
+
+
+# functionality to move the snake
 def move_up():
-    if my_snake.direction != "down":
-        my_snake.direction = "up"
+    if snake_head.direction != "down":
+        snake_head.direction = "up"
 
 def move_down():
-    if my_snake.direction != "up":
-        my_snake.direction = "down"
+    if snake_head.direction != "up":
+        snake_head.direction = "down"
 
 def move_right():
-    if my_snake.direction != "left":
-        my_snake.direction = "right"
+    if snake_head.direction != "left":
+        snake_head.direction = "right"
 
 def move_left():
-    if my_snake.direction != "right":
-        my_snake.direction = "left"
+    if snake_head.direction != "right":
+        snake_head.direction = "left"
 
+
+#function to move the snake
 def snake_movements():
-    if my_snake.direction == "up":
-        y = my_snake.ycor()
-        my_snake.sety(y + 20)
+    if snake_head.direction == "up":
+        y = snake_head.ycor()
+        snake_head.sety(y + 20)
 
-    if my_snake.direction == "down":
-        y = my_snake.ycor()
-        my_snake.sety(y - 20)
+    if snake_head.direction == "down":
+        y = snake_head.ycor()
+        snake_head.sety(y - 20)
 
-    if my_snake.direction == "right":
-        x = my_snake.xcor()
-        my_snake.setx(x + 20)
+    if snake_head.direction == "right":
+        x = snake_head.xcor()
+        snake_head.setx(x + 20)
 
-    if my_snake.direction == "left":
-        x = my_snake.xcor()
-        my_snake.setx(x - 20)
+    if snake_head.direction == "left":
+        x = snake_head.xcor()
+        snake_head.setx(x - 20)
 
 # keyboard operations
 screen.listen()
@@ -85,90 +87,102 @@ screen.onkeypress(move_down, "s")
 screen.onkeypress(move_right, "d")
 screen.onkeypress(move_left, "a")
 
-# looping
-while True:
-    snake_movements()  # Move the snake before updating the screen
-    screen.update()
+segments = []
 
-    # check collision with border
+# Main game loop
+while True:
+    
+    screen.update()
+    #check collision with border
     if (
-        my_snake.xcor() > 290
-        or my_snake.xcor() < -290
-        or my_snake.ycor() > 290
-        or my_snake.ycor() < -290
+        snake_head.xcor() > 290
+        or snake_head.xcor() < -290
+        or snake_head.ycor() > 290
+        or snake_head.ycor() < -290
     ):
         time.sleep(1)
-        my_snake.goto(0, 0)
-        my_snake.direction = "stop"
+        snake_head.goto(0, 0)
+        snake_head.direction = "stop"
 
         # hide body segments
         for segment in segments:
             segment.goto(1000, 1000)
         segments.clear()
-
-        # reset score and delay
-        my_score = 0
+        score = 0
         delay = 0.1
-
         board.clear()
         board.write("Score: {} High Score: {}".format(my_score, high_scores), align="center",
                     font=("ds-digital", 24, "normal"))
-
-    # check collision with the food
-    if my_snake.distance(prey) < 20:
-        x = random.randint(-290, 290)
-        y = random.randint(-290, 290)
+        
+        # check collision with the food
+    if snake_head.distance(prey) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
         prey.goto(x, y)
 
         # add a new segment to the head
-        add_segment = turtle.Turtle()
-        add_segment.speed(0.01)
-        add_segment.shape("circle")
-        add_segment.color("blue")
-        add_segment.penup()
-        segments.append(add_segment)
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("circle")
+        new_segment.color("blue")
+        new_segment.penup()
+        segments.append(new_segment)
 
         # shorten the delay time
         delay -= 0.001
-        # increase the score
+        #increase the score
         my_score += 10
 
         if my_score > high_scores:
             high_scores = my_score
         board.clear()
         board.write("Score: {} High Score: {}".format(my_score, high_scores), align="center",
-                    font=("ds-digital", 24, "normal"))
+            font=("ds-digital", 24, "normal"))
+        
+    # move the segments in reverse
+    for i in range(len(segments) - 1, 0, -1):
+        x = segments[i - 1].xcor()
+        y = segments[i - 1].ycor()
+        segments[i].goto(x, y)
 
-        # move the segments in reverse
-        for i in range(len(segments) - 1, 0, -1):
-            x = segments[i - 1].xcor()
-            y = segments[i - 1].ycor()
-            segments[i].goto(x, y)
-
-        if len(segments) > 0:
-            x = my_snake.xcor()
-            y = my_snake.ycor()
-            segments[0].goto(x, y)
+    #move the 1st segment to where the head is
+    if len(segments) &AMPER:
+        x = snake_head.xcor()
+        y = snake_head.ycor()
+        segments[0].goto(x,y)
+    snake_movements() 
 
     # check for collision with the body
     for segment in segments:
-        if segment.distance(my_snake) < 20:
+        if segment.distance(snake_head) < 20:
             time.sleep(1)
-            my_snake.goto(0, 0)
-            my_snake.direction = "stop"
+            snake_head.goto(0, 0)
+            snake_head.direction = "stop"
 
             for segment in segments:
                 segment.goto(1000, 1000)
             segments.clear()
-            my_score = 0
-            delay = 0.1
 
             # update the score
+            my_score = 0
+            delay = 0.1
             board.clear()
             board.write("Score: {} High Score: {}".format(my_score, high_scores), align="center",
-                        font=("ds-digital", 24, "normal"))
+                font=("ds-digital", 24, "normal"))
 
     time.sleep(delay)
+
+
+
+
+
+
+
+
+        
+
+
+
 
 
             
